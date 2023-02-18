4075eaabc60660e3a5a4cd9c730450c2c87b17df
Shreyash <114304964+Shreyas0620@users.noreply.github.com>
2023-02-18 18:24:42 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,47 @@
+// Server Creation 
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 80;
+
+// const server = http.createServer((req, res) => {
+//   res.end('THIS SERVER IS CREATED BY SHREYAS , Now you Make Changes ');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// Read File module 
+
+// const fs = require('fs')
+
+// const text = fs.readFileSync("delete.txt", "utf-8")
+
+// console.log(text)
+
+
+
+
+
+// file creation and delete as well 
+// const fs = require('fs')
+
+//  const data = fs.writeFile('tut.txt',"This Is a text",(err,data)=>{
+// console.log('file is created')
+// })
+
+// fs.unlink('tut.txt',()=>{
+//   console.log('file is deleted')
+// })
+
+
+
+
+// Synchronus or Blocking Call 
+// In this you will find line by line execution 
+
+// Asynchronus or Non-Blocking Call 
+// CallBack will use here  
---
+++ new.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+const text = fs.readFileSync("delete.txt", "utf-8")
+
+console.log(new_text)
